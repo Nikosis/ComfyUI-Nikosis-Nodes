0dc02f31acc1e415af4adbacc91f30f728db5f25
Nikosis <nikosis.dev@gmail.com>
2025-03-12 19:42:16 -0500

    restructure init file for better clarity

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,14 +1,19 @@
 # Import mappings from individual node files
 
-from .prompt_multiple_styles_selector_nikosis import NODE_CLASS_MAPPINGS as PMSS_NODE_CLASS, NODE_DISPLAY_NAME_MAPPINGS as PMSS_NODE_DISPLAY
-from .text_concatenate_nikosis import NODE_CLASS_MAPPINGS as TC_NODE_CLASS, NODE_DISPLAY_NAME_MAPPINGS as TC_NODE_DISPLAY
-from .aspect_ratio_nikosis import NODE_CLASS_MAPPINGS as AR_NODE_CLASS, NODE_DISPLAY_NAME_MAPPINGS as AR_NODE_DISPLAY
+from .aspect_ratio_nikosis import AspectRatioNikosis
+from .text_concatenate_nikosis import TextConcatenateNikosis
+from .prompt_multiple_styles_selector_nikosis import PromptMultipleStylesSelectorNikosis
 
-# Combine all node class mappings into a single dictionary
-NODE_CLASS_MAPPINGS = { **PMSS_NODE_CLASS, **TC_NODE_CLASS, **AR_NODE_CLASS }
+NODE_CLASS_MAPPINGS = {
+    "AspectRatioNikosis": AspectRatioNikosis,
+    "TextConcatenateNikosis": TextConcatenateNikosis,
+    "PromptMultipleStylesSelectorNikosis": PromptMultipleStylesSelectorNikosis,
+}
 
-
-# Combine all node display name mappings into a single dictionary
-NODE_DISPLAY_NAME_MAPPINGS = { **PMSS_NODE_DISPLAY, **TC_NODE_DISPLAY, **AR_NODE_DISPLAY }
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AspectRatioNikosis": "Aspect Ratio (nikosis)",
+    "TextConcatenateNikosis": "Text Concatenate (nikosis)",
+    "PromptMultipleStylesSelectorNikosis": "Prompt Multiple Styles Selector (nikosis)",
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
